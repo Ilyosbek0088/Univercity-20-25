fe632e5f411b98f349ad0511dc60b36641705ec8
Ilyosbek0088 <ilyosbek0088@gmail.com>
2025-01-24 23:47:41 +0500

    new items

--- src/Pages/community/community.js
+++ src/Pages/community/community.js
@@ -6,7 +6,7 @@ import Card3 from '../../components/ui/cardv3'
 const Community = () => {
   return (
     <div className='container w-full flex justify-center items-center'>
-    <div className='com-img  w-full flex items-center justify-center gap-x-5 bg-black '>
+    <div className='com-img  w-full flex items-center justify-center gap-x-5  bg-black '>
       <Card/>
       <Card2/>
       <Card3/>
--- src/components/ui/cardv2.jsx
+++ src/components/ui/cardv2.jsx
@@ -1,29 +1,31 @@
 import React from "react";
-import calendary from "../../assets/calendary-icon-com.png";
+import calendary from '../../assets/calendary-icon-com.png'
 
 const Card2 = () => {
   return (
-    <div className="w-full max-w-sm md:max-w-md lg:max-w-lg bg-white border rounded-sm p-6 shadow-md text-center flex flex-col md:flex-row gap-x-4">
-      {/* Icon Section */}
-      <div className="flex justify-center items-center mb-4 md:mb-0 md:w-1/3">
-        <img src={calendary} alt="Calendar icon" className="w-12 md:w-16 lg:w-20" />
+    <div className="w-80 bg-[white] border gap-x-2 rounded-sm p-6 shadow-md text-center flex">
+
+{/* ong tomoni  */}
+
+      <div className="flex justify-center mb-4 mt-1 text-4xl text-gray-600 ">
+        <span role="img" aria-label="search">
+          <img src={calendary} className="w-24" ></img>
+        </span>
       </div>
 
-      {/* Text Section */}
-      <div className="text-left md:w-2/3">
-        <h3 className="text-lg font-semibold text-gray-800 mb-2 lg:text-xl">
-          Find a course
-        </h3>
-        <p className="text-sm text-gray-600 mb-4 lg:text-base">
-          Search by subject, course, or region to find the right course for you.
-        </p>
-        <a
-          href="#"
-          className="text-yellow-500 font-semibold text-sm lg:text-base hover:underline"
-        >
-          GET STARTED →
-        </a>
-      </div>
+{/* chap tomni uzb sila */}
+
+<div className="">
+      <h3 className="text-lg font text-left text-gray-800 mb-2">Find a course</h3>
+      <p className="text-sm text-gray-600 mb-4 text-left text-[15px]">
+        Search by subject, course or region to find the right course for you.
+      </p>
+      <a href="#" className= " text-yellow-500 text-left font-semibold text-sm hover:underline" >
+        GET STARTED →
+      </a> 
+</div>
+
+
     </div>
   );
 };
