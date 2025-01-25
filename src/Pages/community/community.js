49fc344940050f9c3db031a4831e31c804c4e712
Ilyosbek0088 <ilyosbek0088@gmail.com>
2025-01-25 14:37:07 +0500

    ssdd

--- src/Pages/community/community.js
+++ src/Pages/community/community.js
@@ -5,11 +5,13 @@ import Card3 from '../../components/ui/cardv3'
 
 const Community = () => {
   return (
-    <div className='container w-full flex justify-center items-center'>
+    <div className='w-full flex items-center justify-center '>
+    <div className='container w-full max-w-[1450px] flex justify-center items-center'>
     <div className='com-img  w-full flex items-center justify-center gap-x-5  bg-black '>
       <Card/>
       <Card2/>
       <Card3/>
+    </div>
     </div>
     </div>
   )
--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -4,7 +4,7 @@ import '../../index.css'
 
 const Home = () => {
   return (
-    <div className='image container mx-auto w-full max-w-7xl flex justify-center items-center flex-col gap-y-6'>
+    <div className='image container mx-auto w-full max-w-[1280px] flex justify-center items-center flex-col gap-y-6'>
        <div className='capitalize text-white text-[60px] font-[500]'>When we can <span className='bg-[#E5B300] px-1 rounded-sm'>finally</span> study!!!</div> 
        <div className='capitalize bg-[#ffffff46] backdrop-blur-sm px-1 rounded-sm text-white text-[30px] font-[500]'>free prison escape tutorial</div> 
     </div>
